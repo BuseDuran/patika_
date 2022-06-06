4168f6647e96b582fcc1036aa52d64c815009758
Buse Duran <91667950+BuseDuran@users.noreply.github.com>
2022-06-06 15:18:46 +0300

    Add files via upload

---
+++ Patika/Patika/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+string inputString;
+Console.WriteLine("Hello, World!");
+inputString=Console.ReadLine();
+Console.WriteLine(inputString);
